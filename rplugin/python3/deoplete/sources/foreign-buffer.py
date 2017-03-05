c1c20c3273f2ce9c4f50c4a728a746089a373a7a
Aleks Kamko <aykamko@gmail.com>
2017-03-05 15:40:40 -0800

    simplify, bug fixes

--- rplugin/python3/deoplete/sources/foreign-buffer.py
+++ rplugin/python3/deoplete/sources/foreign-buffer.py
@@ -1,41 +1,5 @@
-import subprocess
-import os
-import memcache
-import time
-
-from os import path as osp
-
-from foreign_buffer import register_client, local_nvim_id
+from foreign_buffer import create_registered_client, LOCAL_NVIM_ID
 from .buffer import Source as BufferSource
-
-MEMCACHED_SOCK_PATH = osp.join(os.getenv('TMPDIR'), 'foreign-buffer-memcache.sock')
-CONNECT_SLEEP = 0.1
-CONNECT_RETRIES = int(10 / CONNECT_SLEEP)
-
-
-class _MemcachedBuffers:
-    def __init__(self, mc_client):
-        self.mc_client = mc_client
-
-    def __contains__(self, item):
-        return (local_nvim_id(), item) in self.get_bufnrs()
-
-    def __setitem__(self, bufnr, value):
-        self.update_bufnrs(bufnr)
-        self.mc_client.set('buf_{}_{}'.format(local_nvim_id(), bufnr), value)
-
-    def values(self):
-        bufnrs = self.get_bufnrs()
-        return (self.mc_client.get('buf_{}_{}'.format(nvim_id, bufnr))
-                for nvim_id, bufnr in bufnrs if nvim_id != local_nvim_id())
-
-    def get_bufnrs(self):
-        return self.mc_client.get('bufnrs') or set()
-
-    def update_bufnrs(self, bufnr):
-        bufnrs = self.get_bufnrs().union({(local_nvim_id(), bufnr)})
-        self.mc_client.set('bufnrs', bufnrs)
-        return bufnrs
 
 
 class Source(BufferSource):
@@ -44,20 +8,33 @@ class Source(BufferSource):
 
         self.name = 'foreign-buffer'
         self.mark = '[fB]'
-        self.mc_client = self.connect_memcached()
-        register_client(self.mc_client)
+        self.mc_client = create_registered_client()
         self.__buffers = _MemcachedBuffers(self.mc_client)
 
-    def connect_memcached(self):
-        if not osp.exists(MEMCACHED_SOCK_PATH):
-            subprocess.call(['memcached', '-A', '-d', '-s', MEMCACHED_SOCK_PATH])
-        client = memcache.Client(['unix:%s' % (MEMCACHED_SOCK_PATH)],
-                                 socket_timeout=10)
-        for _ in range(CONNECT_RETRIES):
-            success = client.servers[0].connect()
-            if success == 1:
-                break
-            client.forget_dead_hosts()
-            time.sleep(CONNECT_SLEEP)
-            # TODO: else, fail
-        return client
+
+def buf_id(nvim_id, bufnr):
+    return 'buf_{}_{}'.format(nvim_id, bufnr)
+
+
+class _MemcachedBuffers:
+    def __init__(self, mc_client):
+        self.mc_client = mc_client
+
+    def __contains__(self, bufnr):
+        return (LOCAL_NVIM_ID, bufnr) in self.get_bufnrs()
+
+    def __setitem__(self, bufnr, value):
+        self.update_bufnrs(bufnr)
+        self.mc_client.set(buf_id(LOCAL_NVIM_ID, bufnr), value)
+
+    def values(self):
+        return (self.mc_client.get(buf_id(nvim_id, bufnr))
+                for nvim_id, bufnr in self.get_bufnrs() if nvim_id != LOCAL_NVIM_ID)
+
+    def get_bufnrs(self):
+        return self.mc_client.get('bufnrs') or set()
+
+    def update_bufnrs(self, bufnr):
+        new_bufnrs = self.get_bufnrs().union({(LOCAL_NVIM_ID, bufnr)})
+        self.mc_client.set('bufnrs', new_bufnrs)
+        return new_bufnrs
--- rplugin/python3/foreign_buffer/__init__.py
+++ rplugin/python3/foreign_buffer/__init__.py
@@ -1,35 +1,61 @@
 import neovim
 import memcache
 import os
+import subprocess
+import time
 
 from os import path as osp
-from functools import lru_cache
 
-memoized = lru_cache(maxsize=None)
+LOCAL_NVIM_ID = os.getenv('NVIM_LISTEN_ADDRESS')
 MEMCACHED_SOCK_PATH = osp.join(os.getenv('TMPDIR'), 'foreign-buffer-memcache.sock')
+CONNECT_SLEEP = 0.1
+CONNECT_RETRIES = int(10 / CONNECT_SLEEP)
 
 
-@memoized
-def local_nvim_id():
-    return os.getenv('NVIM_LISTEN_ADDRESS')
+def create_registered_client():
+    if not osp.exists(MEMCACHED_SOCK_PATH):
+        subprocess.call(['memcached', '-A', '-d', '-s', MEMCACHED_SOCK_PATH])
+    client = memcache.Client(['unix:%s' % (MEMCACHED_SOCK_PATH)],
+                             socket_timeout=10)
+    for _ in range(CONNECT_RETRIES):
+        success = client.servers[0].connect()
+        if success == 1:
+            break
+        client.forget_dead_hosts()
+        time.sleep(CONNECT_SLEEP)
+        # TODO: else, fail
+    register_client(client)
+    return client
 
 
 def register_client(client):
-    '''register this neovim client with memcached'''
-    nvim_clients = (client.get('nvim_clients') or set()).union({local_nvim_id()})
+    '''
+    Register this neovim client with memcached.
+    '''
+    nvim_clients = (client.get('nvim_clients') or set()).union({LOCAL_NVIM_ID})
     client.set('nvim_clients', nvim_clients)
 
 
 def unregister_client(client):
+    '''
+    Unregister this neovim client with memcached,
+    shut down memcached if last exiting client.
+    '''
     nvim_clients = client.get('nvim_clients')
     if not nvim_clients:
         return  # TODO: error
-    nvim_clients.remove(local_nvim_id())
+
+    try:
+        nvim_clients.remove(LOCAL_NVIM_ID)
+    except KeyError:  # happens when we exit nvim before we're even able to register
+        pass
+
     if not nvim_clients or not len(nvim_clients):
         # we're the last nvim client to exit so we kill memcached too
         client.servers[0].send_cmd('shutdown')
         os.remove(MEMCACHED_SOCK_PATH)
         return
+
     client.set('nvim_clients', nvim_clients)
 
 
